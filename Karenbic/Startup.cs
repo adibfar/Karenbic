c22e715a60bd646b141edfce3e96e70f42b7035d
Emertat89 <ali.andalibi@outlook.com>
2015-04-14 10:46:45 +0430

    Initialize Project

---
+++ Karenbic/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Karenbic.Startup))]
+namespace Karenbic
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+
+        }
+    }
+}

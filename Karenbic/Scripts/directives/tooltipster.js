98ec22c1c4f8e1ec9fbc8672a16d57951f0ea0ba
Emertat89 <ali.andalibi@outlook.com>
2015-08-16 17:34:12 +0430

    textbox focus

--- Karenbic/Scripts/directives/tooltipster.js
+++ Karenbic/Scripts/directives/tooltipster.js
@@ -10,12 +10,31 @@ App.directive('tooltipster', function () {
             if (attrs["tooltipsterTitle"] != undefined && attrs["tooltipsterTitle"] != null) {
                 $(element).tooltipster({
                     content: $(attrs["tooltipsterTitle"]),
-                    theme: 'tooltipster-light'
+                    theme: 'tooltipster-light',
+                    trigger: 'custom'
+                }).on('focus', function () {
+                    $(this).tooltipster('show');
+                }).on('blur', function () {
+                    $(this).tooltipster('hide');
+                }).on('hover', function () {
+                    $(this).tooltipster('show');
+                }).on('mouseleave', function () {
+                    if ($(element).is(':focus') == false)
+                        $(this).tooltipster('hide');
                 });
             }
             else {
                 $(element).tooltipster({
                     theme: 'tooltipster-light'
+                }).on('focus', function () {
+                    $(this).tooltipster('show');
+                }).on('blur', function () {
+                    $(this).tooltipster('hide');
+                }).on('hover', function () {
+                    $(this).tooltipster('show');
+                }).on('mouseleave', function () {
+                    if ($(element).is(':focus') == false)
+                        $(this).tooltipster('hide');
                 });
             }
         }
